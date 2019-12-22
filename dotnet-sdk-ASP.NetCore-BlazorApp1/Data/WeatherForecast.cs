97d313b9a1e8865a492ab7bc2024518479e74885
Nico Leputois <nicoleputois@outlook.com>
2019-12-22 02:58:35 +0100

    Push

---
+++ dotnet-sdk-ASP.NetCore-BlazorApp1/Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace dotnet_sdk_ASP.NetCore_BlazorApp1.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
